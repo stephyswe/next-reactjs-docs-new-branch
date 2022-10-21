24b76a888f2ac476d5a7bb84877f29cefa8efb44
Stephanie <stephaniiehallberg@gmail.com>
2022-10-21 02:36:24 +0200

    feat(content): li, ul and a tags

--- src/components/MDX/MDXComponents.tsx
+++ src/components/MDX/MDXComponents.tsx
@@ -4,15 +4,38 @@
 
 import * as React from 'react';
 import {H2} from './Heading';
+import Link from './Link';
 import HomepageHero from './HomepageHero';
 
 const P = (p: JSX.IntrinsicElements['p']) => (
   <p className="whitespace-pre-wrap my-4" {...p} />
 );
 
+const LI = (p: JSX.IntrinsicElements['li']) => (
+  <li className="leading-relaxed mb-1" {...p} />
+);
+const UL = (p: JSX.IntrinsicElements['ul']) => (
+  <ul className="ml-6 my-3 list-disc" {...p} />
+);
+
+function LinkWithTodo({href, children, ...props}: JSX.IntrinsicElements['a']) {
+  if (href?.startsWith('TODO')) {
+    return children;
+  }
+
+  return (
+    <Link href={href} {...props}>
+      {children}
+    </Link>
+  );
+}
+
 export const MDXComponents = {
   p: P,
+  ul: UL,
+  li: LI,
   h2: H2,
+  a: LinkWithTodo,
   MaxWidth({children}: {children: any}) {
     return <div className="max-w-4xl ml-0 2xl:mx-auto">{children}</div>;
   },
